2e24162d670f27d690de02549f944e6c6cc5a449
NikitBogdanov <92532985+NikitBogdanov@users.noreply.github.com>
2022-10-10 18:50:31 +0300

    KompGraf-1

---
+++ KompGraf№1/KompGraf№1/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KompGraf_1
+{
+    public partial class Form1 : Form
+    {
+        Graphics graphics;
+        public Form1()
+        {
+            InitializeComponent();
+            graphics = pictureBox1.CreateGraphics();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Pen myPen = new Pen(Color.Blue, 5);
+                graphics.DrawRectangle(myPen, 0, 0, pictureBox1.Size.Width - 1, pictureBox1.Size.Height - 1);
+                graphics.Dispose();
+                Form2 form2 = new Form2();
+                form2.Show();
+            }
+            catch { }
+        }
+    }
+}
---
+++ KompGraf№1/KompGraf№1/Form2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KompGraf_1
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            // Получаем объект Graphics
+            Graphics g = e.Graphics;
+            g.DrawLine(Pens.Red, 100, 105, 180, 220);
+            g.DrawEllipse(Pens.Blue, 10, 20, 110, 45);
+            g.DrawRectangle(Pens.Green, 10, 70, 110, 45);
+            g.FillEllipse(Brushes.Blue, 130, 20, 110, 45);
+            g.FillRectangle(Brushes.Green, 130, 70, 110, 45);
+        }
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
